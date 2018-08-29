676c8eee56a239b6b16aac338a7a9548bebe1e69
wac <antoine.leon@epitech.eu>
2018-08-29 15:09:53 +0200

    Added add and delete methods

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,13 +6,55 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  allowTodoCreation = false;
+  todoStatus = false;
+  
+  todoCreationStatus = 'No todo created';
 
-  todoList = [''];
-  @Input() newTodo: string;
+  todos = [];
 
-  constructor() { }
+  newTodo = '';
+
+  constructor() { 
+    setTimeout(() => {
+      this.allowTodoCreation = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
+
+  onCreateTodo(){
+    var tab = 
+      {
+        todoStatus: false,
+        name: this.newTodo
+      }
+      
+      this.todos.push(tab);
+  
+      this.newTodo =''
+}
+
+ 
+  onDeleteTodo(todo,i){
+    this.todos.splice(i,1);
+    
+  }
+
+  deleteSelected(){
+      this.todos = this.todos.filter((todo)=> !todo.todoStatus);
+    }
+
+  // onComplete(event: Event){
+  //   if((<HTMLInputElement>event.target).checked){
+  //     this.todoStatus = true;
+  //     //console.log(this.todoStatus);
+  //   }
+  //   else{
+  //     this.todoStatus = false;
+  //   }
+
+  // }
 
 }
